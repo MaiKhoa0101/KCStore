39e5ba0f27f6a4a44bf7b13f255c37f998514e4f
MaiKhoa0101 <maikhoa2015@gmail.com>
2024-09-16 17:38:49 +0700

    xoa head

--- carts.php
+++ carts.php
@@ -10,8 +10,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <style>
-<<<<<<< HEAD
-=======
         /* khoa depzai */
         .section_account_admin h3 {
             margin-top: 0;
@@ -19,7 +17,6 @@
             color: #333;
         }
 
->>>>>>> a18753c32f48c1cfe8ef75a1012cd440a314f6a8
         .welcome-msg {
             display: block;
             text-align: center;
